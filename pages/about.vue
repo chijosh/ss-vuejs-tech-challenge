86041513f6a37aa8c3ee5f077ad977f9113a2857
Joshua Onyenwere <joshua.onyenwere@bike24.net>
2023-08-15 11:37:36 +0200

    SIDESTREAM-019 Updated About us page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,14 @@
 <template>
-  <p>About Us</p>
+  <div class="mx-4 md:mx-12 lg:mx-24">
+    <h1 class="text-3xl md:text-4xl lg:text-5xl mt-4 mb-2 font-bold">About Us</h1>
+    <div class="my-4">
+      <p class="text-sm md:text-base">
+        This is the overview board of the Cologne Zoo. It can be used by zookeepers to get an
+        overview of the animals and their relevant health data.
+      </p>
+      <img class="w-auto" src="../assets/images/logo.png" alt="Logo" />
+    </div>
+  </div>
 </template>
 
 <script></script>
